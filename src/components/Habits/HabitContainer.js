648318e4bd9cd3f65ce7dedcc93382c864fa612d
Natalie Chen <natalie.chen014@gmail.com>
2022-02-18 13:04:14 -0800

    cleaned up scss files

--- src/components/Habits/HabitContainer.js
+++ src/components/Habits/HabitContainer.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import '../../css/Habits.css';
 
 // components
 import HabitInput from './HabitInput';
--- src/components/Tasks/TaskContainer.js
+++ src/components/Tasks/TaskContainer.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import '../../css/Tasks.css';
 
 // components
 import TaskInput from './TaskInput';
